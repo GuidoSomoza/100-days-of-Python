a7758ff88dc01303f889696e1fa69521c0db9383
GuidoSomoza <gsomoza1404@gmail.com>
2024-09-23 20:58:43 -0300

    2nd day commit

--- Day_2/BillCalculator.py
+++ Day_2/BillCalculator.py
@@ -1,6 +1,10 @@
+print("Welcome to the tip calculator")
 
-print(type("12345"))
-print(type(123))
-print(type(123_456_566))
-print (type(False))
-print(type(2.5))
+total_bill = float(input("What was the total bill? $"))
+tipPercentage = int(input("How much tip would you like to give? 10,12, or 15?"))
+number_of_people= int(input("How many people to split the bill?"))
+finalTip = total_bill * (tipPercentage/100)
+total_bill_with_tip = finalTip + total_bill
+total_bill_per_person = round((total_bill_with_tip / number_of_people),2)
+
+print(f"Each person should pay : $ {total_bill_per_person}")
